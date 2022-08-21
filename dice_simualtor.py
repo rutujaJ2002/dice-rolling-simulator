1be1d51de99fc16022129e92d937ff336456ae89
Rutuja Sanjay Jadhav <82172257+rutujaJ2002@users.noreply.github.com>
2022-08-21 13:40:25 +0530

    Add files via upload

---
+++ dice_simualtor.py
@@ -0,0 +1,48 @@
+import random
+
+x="y"
+
+while x=="y":
+    print("Dice Rolling Simulator")
+    num= random.randint(1,6)
+
+    if num==1:
+        print("-----------")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("-----------")
+    if num==2:
+        print("-----------")
+        print("|         |")
+        print("|  0   0  |")
+        print("|         |")
+        print("-----------")
+    if num==3:
+        print("-----------")
+        print("|0        |")
+        print("|    0    |")
+        print("|        0|")
+        print("-----------")
+    if num==4:
+        print("-----------")
+        print("|  0   0  |")
+        print("|         |")
+        print("|  0   0  |")
+        print("-----------")
+    if num==5:
+        print("-----------")
+        print("|  0   0  |")
+        print("|    0    |")
+        print("|  0   0  |")
+        print("-----------")
+    if num==6:
+        print("-----------")
+        print("|  0   0  |")
+        print("|  0   0  |")
+        print("|  0   0  |")
+        print("-----------")
+
+    x= input("Press y to roll again ")
+
+
